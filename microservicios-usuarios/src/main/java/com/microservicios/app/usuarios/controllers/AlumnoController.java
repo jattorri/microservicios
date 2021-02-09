f7916d8d9e77d3d084850f2f70b86112600becd1
jattorri <jattorri@gmail.com>
2021-02-09 11:04:40 -0300

    init

---
+++ microservicios-usuarios/src/main/java/com/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -0,0 +1,58 @@
+package com.microservicios.app.usuarios.controllers;
+
+import com.microservicios.app.usuarios.models.entity.Alumno;
+import com.microservicios.app.usuarios.services.AlumnoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class AlumnoController {
+	
+    @Autowired
+    private AlumnoService service;
+
+    @GetMapping
+    public ResponseEntity<?> listar(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> ver(@PathVariable Long id){
+        Optional<Alumno> o = service.findById(id);
+        if(o.isPresent()){   return ResponseEntity.ok(o.get());}
+        else {
+           return ResponseEntity.notFound().build();
+        }
+     
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> crear (@RequestBody Alumno alumno){
+        Alumno alumnoDb = service.save(alumno);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
+        Optional<Alumno> o = service.findById(id);
+
+        if(o.isPresent()){}
+        else {
+            return ResponseEntity.notFound().build();
+        }
+
+        Alumno alumnoDb = o.get();
+        alumnoDb.setNombre(alumno.getNombre());
+        alumnoDb.setApellido(alumno.getApellido());
+        alumnoDb.setEmail(alumno.getEmail());
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar (@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
